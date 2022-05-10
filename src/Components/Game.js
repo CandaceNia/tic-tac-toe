202cda8354f4b5804749dbd535683440b58ff4b7
CandaceNia <98069665+CandaceNia@users.noreply.github.com>
2022-05-10 11:14:56 -0400

    .

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -5,9 +5,14 @@ import "./Game.css";
 const board = ["X", null, null, null, "O", "O", null, null, "X"];
 
 class Game extends Component {
-  render() {
-    const squares = board.map((val, ind) => <Square key={ind} value={val} />);
+  constructor(){
+    super();
+    this.state  = {board : ["X", null, null, null, "O", "O", null, null, "X"]}
 
+  } 
+  render() {
+    const squares = this.state.board.map((val, ind) => <Square key={ind} value={val} />);
+console.log(squares)
     return (
       <div className="tic-tac-toe-game">
         <h1>Tic Tac Toe</h1>
